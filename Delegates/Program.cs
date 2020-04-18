2f7d0b9149fb00bf85f60258b8d93428fec4aa8d
JamesLambe3006 <james.lambe@bglgroup.co.uk>
2020-04-18 16:23:44 +0100

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        delegate double ArithmeticOperation(double operand1, double operand2);
+
+        static void Main(string[] args)
+        {
+            ArithmeticOperation sum = (double number1, double number2) =>
+            {
+                return number1 + number2;
+            };
+
+            ArithmeticOperation minus = (double number1, double number2) =>
+            {
+                return number1 - number2;
+            };
+
+            Print(sum, 1, 6);
+            Print(sum, 3, 9);
+            Print(minus, 15, 9);
+
+            Console.WriteLine();
+        }
+
+        static void Print(ArithmeticOperation operation, double num1, double num2)
+        {
+            Console.WriteLine($"Answer is: {operation(num1, num2)}");
+        }
+    }
+}
